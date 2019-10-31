8dc6e1b84d447a3a3bcfed15e1d3afbe4d40a4da
alindnbrg <mail@andrelindenberg.de>
2019-10-31 14:03:20 +0100

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by André Lindenberg on 31.10.19.
+//  Copyright © 2019 alindnbrg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
